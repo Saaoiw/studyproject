36b6668441ab8aa04b5503fb2218f57f4fe22ed3
Saaoiw <Saaoiw@gmail.com>
2016-11-02 23:13:04 +0200

    new build

--- src/main/java/com/brainacad/studyproject/Application.java
+++ src/main/java/com/brainacad/studyproject/Application.java
@@ -1,8 +1,20 @@
 package com.brainacad.studyproject;
 
+import com.brainacad.studyproject.data.core.StubDataHolder;
+import com.brainacad.studyproject.service.impl.LoginServiceImpl;
+
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        //System.out.println("Hello!");
+        StubDataHolder.createData();
+        LoginServiceImpl loginService = new LoginServiceImpl();
+        if (loginService.login("qweqw", "qweqweqwe")) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Login failed");
+        }
     }
+
 }
